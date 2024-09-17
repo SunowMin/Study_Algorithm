55f79bb98077a62dfba22a0f6fd58fd375cf15fd
Sumin Kim <kko3093@gmail.com>
2024-09-18 04:06:40 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
- 
-int count(string str);
-int main(){
-    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
-    cin.tie(NULL);	// 입력과 출력 묶음을 풀기
- 
-    string s;
-    cin >> s;    
-    cout << count(s);
-    return 0;
+
+int count(string str) {
+	string alpha[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+	for (int i = 0; i < 8; i++) {
+		while (1) {
+			// find : 시작 위치를 리턴
+			if (str.find(alpha[i]) != -1) {
+				// replace (교체원하는 문자열의 위치, 교체원하는 문자열의 길이, 새로운 문자열)
+				str.replace(str.find(alpha[i]), alpha[i].length(), "*");
+			}
+			else break;
+		}
+	}
+
+	return str.length();
 }
- 
-int count(string str){
-    string alphabet[8] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
- 
-    for(int i=0; i<8; i++){
-        while(1){
-            if(str.find(alphabet[i]) != -1){
-                str.replace(str.find(alphabet[i]), alphabet[i].length(), "#");
-            }else break;
-        }
-    }
-    return str.length();
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string word;
+	int cro = 0;
+
+	cin >> word;
+
+	cout << count(word);
+
+	return 0;
 }
