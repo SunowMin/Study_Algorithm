95650c6c8d77ae999d5ce21cb93476c98b7f3376
Sumin Kim <kko3093@gmail.com>
2024-10-15 01:26:10 +0900

    [Bronze IV] Title: Which Alien?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int an, eye;
+
+	cin >> an >> eye;
+
+	if (an >= 3 && eye <= 4) {
+		cout << "TroyMartian" << "\n";
+	}
+	if (an <= 6 && eye >= 2) {
+		cout << "VladSaturnian" << "\n";
+	}
+	if (an <= 2 && eye <= 3) {
+		cout << "GraemeMercurian" << "\n";
+	}
+
+	return 0;
+}
