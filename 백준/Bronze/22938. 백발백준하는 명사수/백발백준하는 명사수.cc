47a7396e756eb02fb9f46fa4b4c71cf1232f0f66
Sumin Kim <kko3093@gmail.com>
2024-12-17 23:46:13 +0900

    [Bronze III] Title: 백발백준하는 명사수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/22938. 백발백준하는 명사수/백발백준하는 명사수.cc
+++ 백준/Bronze/22938. 백발백준하는 명사수/백발백준하는 명사수.cc
@@ -1,24 +1,29 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
-int main() {
-    int X1, Y1, R1;  // 첫 번째 과녁의 중심 좌표와 반지름
-    int X2, Y2, R2;  // 두 번째 과녁의 중심 좌표와 반지름
+int main(void) {
 
-    cin >> X1 >> Y1 >> R1;  // 첫 번째 과녁 입력
-    cin >> X2 >> Y2 >> R2;  // 두 번째 과녁 입력
+    int x1, y1, r1, x2, y2, r2;
 
-    // 중심 간 거리의 제곱 계산 (제곱근을 사용하지 않아도 비교 가능)
-    long long distanceSquared = (long long)(X2 - X1) * (X2 - X1) + (long long)(Y2 - Y1) * (Y2 - Y1);
-    long long radiusSum = (long long)(R1 + R2);  // 반지름의 합
-    long long radiusSumSquared = radiusSum * radiusSum;  // 반지름 합의 제곱
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
-    if (distanceSquared < radiusSumSquared) {
-        cout << "YES" << endl;  // 두 원이 겹치는 경우
-    } else {
-        cout << "NO" << endl;   // 두 원이 겹치지 않는 경우 (한 점에서 만나거나 안 만남)
+
+    // 과녁의 중심을 기준으로 두 과녁의 거리 구하기
+    long long centerDistance, xDistance, yDistance;
+    xDistance = x1 - x2;
+    yDistance = y1 - y2;
+
+    centerDistance = (long long)(xDistance * xDistance) + (long long)(yDistance * yDistance);
+
+    // 겹치려면, 중심사이거리 < 두 반지름의 합
+    if (centerDistance < (long long)(r1 + r2) * (r1 + r2)) {
+        cout << "YES";
     }
+    else {
+        cout << "NO";
+    }
+
 
     return 0;
 }
