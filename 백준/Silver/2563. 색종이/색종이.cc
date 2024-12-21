cc6f340c8b22b6a18a48b67ffd1c867400939a83
Sumin Kim <kko3093@gmail.com>
2024-12-22 00:22:04 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -1,19 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int a[100][100];
-int main(){
-    int n,x,y,cnt=0;
-    cin>>n;
-    while(n--){
-        cin>>x>>y;
-        for(int i=y;i<y+10;i++){
-            for(int j=x;j<x+10;j++){
-                if(!a[i][j]){
-                    cnt++;
-                    a[i][j]=1;
-                }
+
+int main(void) {
+
+    // 100*100 흰색 도화지를 0으로 세팅
+    char array[100][100] = { 0 };
+    int num, x, y, sum = 0;
+
+    cin >> num;
+    for (int i = 0; i < num; i++) {
+        cin >> x >> y;
+        for (int j = x; j < x + 10; j++) {
+            for (int k = y; k < y + 10; k++) {
+                array[j][k] = 1;
             }
         }
     }
-    cout<<cnt;
+
+    // 1의 개수 출력
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (array[i][j] == 1) {
+                sum += 1;
+            }
+        }
+    }
+    cout << sum;
+
+    return 0;
 }
